fa769a7aec74fad2c1eea0bf2f55f23c9df52dd5
Nico Bobb <nicobobb@gmail.com>
2023-11-28 19:58:17 -0300

    Add button for handle information for first form

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,47 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  function obtenerDatosFormulario(formId) {
+    var formulario = document.getElementById(formId);
+    var datos = {};
 
+    formulario.querySelectorAll("input").forEach(function (input) {
+      datos[input.id] = input.value;
+    });
 
-    function obtenerDatosFormulario(formId) {
-        var formulario = document.getElementById(formId);
-        var datos = {};
+    return datos;
+  }
 
-        formulario.querySelectorAll('input').forEach(function (input) {
-            datos[input.id] = input.value;
-        });
+  window.completarFormulario = function () {
+    var datosIntegrante1 = obtenerDatosFormulario("formIntegrante1");
+    var datosIntegrante2 = obtenerDatosFormulario("formIntegrante2");
 
-        return datos;
+    console.log("-----");
+    console.log(
+      "Integrante 1:",
+      obtenerNombreCompletoFormulario(datosIntegrante1)
+    );
+    console.log(
+      "Integrante 2:",
+      obtenerNombreCompletoFormulario(datosIntegrante2)
+    );
+    console.log("-----");
+
+    var compararApellidos = confirm("¿Deseas comparar los apellidos?");
+    if (compararApellidos) {
     }
+  };
 
-    window.completarFormulario = function () {
-        var datosIntegrante1 = obtenerDatosFormulario('formIntegrante1');
-        var datosIntegrante2 = obtenerDatosFormulario('formIntegrante2');
+  function obtenerNombreCompletoFormulario(datos) {
+    var nombres = [datos.nombre, datos.segundoNombre];
+    var apellidos = [datos.apellido, datos.segundoApellido];
 
-        console.log('-----');
-        console.log('Integrante 1:', obtenerNombreCompletoFormulario(datosIntegrante1));
-        console.log('Integrante 2:', obtenerNombreCompletoFormulario(datosIntegrante2));
-        console.log('-----');
+    nombres = nombres.filter(function (nombre) {
+      return nombre.trim() !== "";
+    });
 
+    apellidos = apellidos.filter(function (apellido) {
+      return apellido.trim() !== "";
+    });
 
-        var compararApellidos = confirm('¿Deseas comparar los apellidos?');
-        if (compararApellidos) {
-          
-        }
-    };
+    var nombreCompleto = nombres.join(" ") + " " + apellidos.join(" ");
 
-    function obtenerNombreCompletoFormulario(datos) {
-        var nombres = [datos.nombre, datos.segundoNombre];
-        var apellidos = [datos.apellido, datos.segundoApellido];
+    return nombreCompleto.toUpperCase();
+  }
+});
 
-        nombres = nombres.filter(function (nombre) {
-            return nombre.trim() !== "";
-        });
+handleSubmitForm1.addEventListener("click", () => {
+  let nombre = document.getElementById("nombre1").value;
+  let segundoNombre = document.getElementById("segundoNombre1").value;
+  let apellido = document.getElementById("apellido1").value;
+  let segundoApellido = document.getElementById("segundoApellido1").value;
 
-        apellidos = apellidos.filter(function (apellido) {
-            return apellido.trim() !== "";
-        });
+  let datos = {
+    nombre,
+    segundoNombre,
+    apellido,
+    segundoApellido,
+  };
 
-        var nombreCompleto = nombres.join(' ') + ' ' + apellidos.join(' ');
-
-        return nombreCompleto.toUpperCase(); 
-    }
+  console.log(datos);
 });
